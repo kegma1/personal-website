36b1deb78e6868d3818f29fc0f0da1fafaaf4ef8
Kennet Mathisen <kennetam02@gmail.com>
2026-07-11 13:39:39 +0200

    first commit

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  description = "Go website development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            go
+            gopls          # Go language server
+            delve          # Debugger
+            gotools        # goimports, etc.
+            air            # Live reload
+            golangci-lint  # Linter
+          ];
+
+          shellHook = ''
+            export GOPATH="$PWD/.gopath"
+            export GOBIN="$GOPATH/bin"
+            export PATH="$GOBIN:$PATH"
+
+            echo "Go $(go version)"
+          '';
+        };
+      });
+}
